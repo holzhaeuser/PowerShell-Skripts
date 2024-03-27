c8d40285f9b25519406ec2f436bd140defe2b649
holzhaeuser <104066426+holzhaeuser@users.noreply.github.com>
2024-03-27 09:13:23 +0100

    Überflüssige Elemente gelöscht.

--- DISM/Test-Image_verkl.ps1
+++
@@ -1,6 +0,0 @@
-Export-WindowsImage -SourceImagePath "E:\sources\install.wim" -SourceIndex 4 -CompressionType max -DestinationImagePath "D:\dism\export.wim"
-New-Item -Name "_Mount" -ItemType Directory -Path "D:\"
-Mount-WindowsImage -ImagePath "D:\dism\export.wim" -Index 1 -Path "D:\_Mount"
-Repair-WindowsImage -Path "D:\_Mount" -StartComponentCleanup -ResetBase
-Dismount-WindowsImage -Path "D:\_Mount" -Save
-Remove-Item -Path "D:\_Mount"
--- DISM/TestCmdLets.ps1
+++
@@ -1,6 +0,0 @@
-if ("{{ vswitch_type }}" -eq "Private" -or "{{ vswitch_type }}" -eq "Internal") {
-    New-VMSwitch -Name "{{ vswitch_name }}" -SwitchType "{{ vswitch_type }}"
-}
-else {
-    New-VMSwitch -Name "{{ vswitch_name }}" -AllowManagementOS $True -NetAdapterName "{{ netadapter_name }}"
-}
